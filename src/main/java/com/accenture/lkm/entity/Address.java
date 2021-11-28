1e12a5ab8c2b5b7b2088bc832274f3f33afce410
brahmatejadarisi2000 <brahmateja.darisi@gmail.com>
2021-11-28 18:52:25 +0530

    Modified Fields in Table(Employee)

--- src/main/java/com/accenture/lkm/entity/Address.java
+++ src/main/java/com/accenture/lkm/entity/Address.java
@@ -8,79 +8,65 @@ public class Address {
 	 private int houseNumber;
 	     
 	   
-	    private String streetAddress;
+	    
 	     
 	    
 	    private String city;
 	     
 	   
-	    private String state;
+	    private String shopname;
 	     
 	   
-	    private String zipCode;
+	    
 public Address()
 {
 	
 }
 
 
-		public Address(int houseNumber, String streetAddress, String city, String state, String zipCode) {
-			super();
-			this.houseNumber = houseNumber;
-			this.streetAddress = streetAddress;
-			this.city = city;
-			this.state = state;
-			this.zipCode = zipCode;
-		}
 
-
-		public int getHouseNumber() {
-			return houseNumber;
-		}
-
-
-		public void setHouseNumber(int houseNumber) {
-			this.houseNumber = houseNumber;
-		}
-
-
-		public String getStreetAddress() {
-			return streetAddress;
-		}
-
-
-		public void setStreetAddress(String streetAddress) {
-			this.streetAddress = streetAddress;
-		}
-
-
-		public String getCity() {
-			return city;
-		}
-
-
-		public void setCity(String city) {
-			this.city = city;
-		}
-
-
-		public String getState() {
-			return state;
-		}
-
-
-		public void setState(String state) {
-			this.state = state;
-		}
-
-
-		public String getZipCode() {
-			return zipCode;
-		}
-
-
-		public void setZipCode(String zipCode) {
-			this.zipCode = zipCode;
-		}
-
+public Address(int houseNumber, String city, String shopname) {
+	super();
+	this.houseNumber = houseNumber;
+	this.city = city;
+	this.shopname = shopname;
 }
+
+
+
+public int getHouseNumber() {
+	return houseNumber;
+}
+
+
+
+public void setHouseNumber(int houseNumber) {
+	this.houseNumber = houseNumber;
+}
+
+
+
+public String getCity() {
+	return city;
+}
+
+
+
+public void setCity(String city) {
+	this.city = city;
+}
+
+
+
+public String getShopname() {
+	return shopname;
+}
+
+
+
+public void setShopname(String shopname) {
+	this.shopname = shopname;
+}
+
+
+		
