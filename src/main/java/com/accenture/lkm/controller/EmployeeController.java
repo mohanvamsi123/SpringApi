dc9d06db63c7297ee4c1ba7f97012fd18ff0a4e2
Mohan Vamsi <mohhanvamsi.d1998@gmail.com>
2021-11-28 17:44:34 +0530

    Update EmployeeController.java

--- src/main/java/com/accenture/lkm/controller/EmployeeController.java
+++ src/main/java/com/accenture/lkm/controller/EmployeeController.java
@@ -2,7 +2,7 @@ package com.accenture.lkm.controller;
 
 import java.util.Collection;
 import java.util.List;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
