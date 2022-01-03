0af1bcf56da717924894835c8721af58478c13dd
Brahmateja2000 <60615398+brahmatejadarisi2000@users.noreply.github.com>
2022-01-03 13:26:33 +0530

    New Up[date Person entity

--- src/main/java/com/accenture/lkm/entity/Person.java
+++ src/main/java/com/accenture/lkm/entity/Person.java
@@ -5,7 +5,7 @@ import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
@@ -41,7 +41,7 @@ public class Person {
 
     @PrePersist
     private void OnCreate(){
-    Createddate=new date();
+    CreatedDate=new date();
      }
    
    
