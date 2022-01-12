4619b7e4b1c5c2eae933793c68d27501a7714cc4
Brahmateja2000 <60615398+brahmatejadarisi2000@users.noreply.github.com>
2022-01-12 19:31:58 +0530

    DAO3

--- src/main/java/com/accenture/lkm/controller/PriceController.java
+++ src/main/java/com/accenture/lkm/controller/PriceController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import com.accenture.lkm.entity.Price;
+import com.accenture.lkm.entity.*;
 import com.accenture.lkm.dao.*;
 
 @CrossOrigin
