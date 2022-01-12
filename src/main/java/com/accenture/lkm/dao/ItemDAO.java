2691f8e53a7050778940f9d827ac205748d46d0d
Brahmateja2000 <60615398+brahmatejadarisi2000@users.noreply.github.com>
2022-01-12 22:09:25 +0530

    loii

--- src/main/java/com/accenture/lkm/dao/ItemDAO.java
+++ src/main/java/com/accenture/lkm/dao/ItemDAO.java
@@ -21,7 +21,7 @@ public interface ItemDAO extends JpaRepository<Item, Long>{
 	@Query("select i from Item i where i.Item_Status= ?1")
 	List<Item> Find(boolean status); 
 
-
+  @Query("select p from Item p  where p.Item_Id=:id")
    public Item findById(Long id);
 
    /*@Modifying
