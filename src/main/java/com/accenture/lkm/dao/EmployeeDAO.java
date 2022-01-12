92f4a81c061dded3eaa4498802e169273aa8e7e2
Brahmateja2000 <60615398+brahmatejadarisi2000@users.noreply.github.com>
2022-01-12 22:05:20 +0530

    ldonre

--- src/main/java/com/accenture/lkm/dao/EmployeeDAO.java
+++ src/main/java/com/accenture/lkm/dao/EmployeeDAO.java
@@ -9,12 +9,12 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.accenture.lkm.entity.Person;
 
-public interface EmployeeDAO  extends JpaRepository<Person, long>{
+public interface EmployeeDAO  extends JpaRepository<Person, Long>{
 	
 	@Transactional
 	 @Modifying
 	@Query("delete from Person p  where p.U_id=:id")
-	int delete(@Param("id") int id);
+	void delete(@Param("id") int id);
 	
 	@Query("select p from Person p  where p.U_id=:id")
 	Person findById(@Param("id") long id);
