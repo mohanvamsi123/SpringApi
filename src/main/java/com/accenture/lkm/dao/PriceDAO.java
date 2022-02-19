ebd251085f24bfcb880569d1b81dbc5e666c5ead
Mohan Vamsi <mohhanvamsi.d1998@gmail.com>
2022-02-19 14:09:48 +0530

    ddd

--- src/main/java/com/accenture/lkm/dao/PriceDAO.java
+++ src/main/java/com/accenture/lkm/dao/PriceDAO.java
@@ -9,5 +9,8 @@ import org.springframework.transaction.annotation.Transactional;
 import com.accenture.lkm.entity.Price;
 public interface PriceDAO extends JpaRepository<Price, Long>{
 
-    
+    @Transactional
+	@Modifying
+	@Query("delete from Price p  where p.Id=:id")
+	void delete(@Param("id") int id);
 }
