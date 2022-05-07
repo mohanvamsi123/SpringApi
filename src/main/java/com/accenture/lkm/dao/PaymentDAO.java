cf14b6e08dccb0710b3ab10945d2c75ee35a994a
Mohan Vamsi <mohhanvamsi.d1998@gmail.com>
2022-05-08 00:43:20 +0530

    New Api to fetch distinct dates and sort prices by ascending

--- src/main/java/com/accenture/lkm/dao/PaymentDAO.java
+++ src/main/java/com/accenture/lkm/dao/PaymentDAO.java
@@ -1,11 +1,23 @@
 package com.accenture.lkm.dao;
 
+import java.util.Date;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 import com.accenture.lkm.entity.Cust_Payment;
-import com.accenture.lkm.entity.Person;
+
 
 public interface PaymentDAO extends JpaRepository<Cust_Payment, Long>{
 	
-
+	 @Transactional
+	 @Modifying
+	 @Query("delete from Cust_Payment p where p.Id=:id")
+	 void delete(@Param("id") long id);
+	 
+	//@Query("select p from Cust_Payment p where p.createdAt>=?1 and p.createdAt<=?2 and p.person.U_id=?3")
+	 @Query("select p from Cust_Payment p where date(convert_tz(p.transaction_date,'+00:00','+05:30'))>=?1 and date(convert_tz(p.transaction_date,'+00:00','+05:30'))<=?2 and p.person.U_id=?3")
+     List<Cust_Payment>findBycreatedAtBetween(Date date1,Date date2,long id);
 }
