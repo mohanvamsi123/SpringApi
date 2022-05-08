f2dcaae8f7c906fec07b2352a7c0ac5a860dcabe
Mohan Vamsi <mohhanvamsi.d1998@gmail.com>
2022-05-08 14:22:26 +0530

    Modified filter by date Query

--- src/main/java/com/accenture/lkm/dao/PriceDAO.java
+++ src/main/java/com/accenture/lkm/dao/PriceDAO.java
@@ -16,9 +16,11 @@ public interface PriceDAO extends JpaRepository<Price, Long>{
 	@Query("delete from Price p  where p.Id=:id")
 	void delete(@Param("id") int id);
 
-    @Query("select p from Price p where p.createdAt>=date(convert_tz(?1,'+00:00','-05:30')) and p.createdAt<=date(convert_tz(?2,'+00:00','-05:30')) and p.person.U_id=?3")
+    @Query("select p from Price p where p.createdAt>=convert_tz(?1,'+00:00','-05:30') and p.createdAt<=convert_tz(?2,'+00:00','-05:30') and p.person.U_id=?3")
     //@Query("select p from Price p where date(convert_tz(p.createdAt,'+00:00','+05:30'))>=?1 and date(convert_tz(p.createdAt,'+00:00','+05:30'))<=?2 and p.person.U_id=?3")
     List<Price>findBycreatedAtBetween(Date date1,Date date2,long id);
+   
+    
 
     @Query("select p from Price p  where p.id=:id")
 	Price findById(@Param("id") long id);
